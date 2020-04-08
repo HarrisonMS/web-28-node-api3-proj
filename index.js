8d1b1fcbea7bdf54c1ca02e4e64b0534df2be559
Harrison <hmseaborn@gmail.com>
2020-04-08 13:57:29 -0700

    server up

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code away!
+const server = require('./server.js');
+
+server.listen(5003, () => {
+  console.log('\n* Server Running on http://localhost:5003 *\n');
+});
---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+
+// module.exports = {
+//   logger
+// };
+
+// function logger(){return function(req, res, next){
+//   if(console.log(`${req.method} to ${req.originalUrl} at ${new Date()}`)){
+//     return next()
+//   }
+// }} 
