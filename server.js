3482f4e690e2f6a87e0354cbdad0103db5962048
Harrison <hmseaborn@gmail.com>
2020-04-08 14:06:19 -0700

    npm i helmet & morgan
    use both plus a logger cause why not

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+const morgan = require('morgan')
+const helmet = require("helmet")
 
 //custom middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method} to ${req.originalUrl} at ${new Date()}`);
+  next();
+}
 
-function logger(req, res, next) {}
+server.use(helmet());
+server.use(morgan('tiny'))
+server.use(logger)
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware! Or take a break we have come so far</h2>`);
+});
 
 module.exports = server;
