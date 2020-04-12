c7b711a5ae5b1acc061c98c823004112b0466659
Harrison <hmseaborn@gmail.com>
2020-04-12 13:32:42 -0700

    get posts and get posts by id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,12 +1,22 @@
 const express = require('express');
-
+const Posts = require('../posts/postDb')
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts.get().then(posts => {
+    res.status(201).json(posts)
+  })
 });
 
 router.get('/:id', (req, res) => {
+  const postId = req.params.id || req.body.user_id;
+  Posts.getById(postId).then(post => {
+    res.status(201).json(post)
+  })
+  .catch((error) => {
+    res.status.json(error)
+  })
   // do your magic!
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const helmet = require("helmet")
 const { logger } = require("./middleware/logger")
 const userRoutes = require('./users/userRouter')
+const postRoutes = require('./posts/postRouter')
 
 //custom middleware
 
@@ -15,6 +16,7 @@ server.use(logger)
 server.use(express.json());
 
 server.use("/api/users", userRoutes)
+server.use("/api/posts", postRoutes)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware! Or take a break we have come so far</h2>`);
